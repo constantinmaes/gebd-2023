74ab6db35fc083946fd357322b621751d90c35c7
Maes Constantin <constantinmaes@gmail.com>
2023-04-11 18:11:45 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,58 @@
+<?php
+
+try {
+    $db = new PDO('mysql:dbname=gebd;host=127.0.0.1', 'root', 'notSecureChangeMe');
+} catch (Exception $e) {
+    echo $e;
+}
+
+$urlArray = explode('/', $_SERVER['REQUEST_URI']);
+
+$cleanedArray = array_slice($urlArray, 2);
+
+// url se termine par index.php ou index.php/
+if (count($cleanedArray) === 0 || $cleanedArray[0] === "") {
+    echo 'Homepage';
+    return;
+}
+
+// Récupération de la ressource (type de données)
+$resource = $cleanedArray[0];
+
+// users/1 => affiche 1 utilisateur
+// users/1/edit => formulaire d'édition de l'utilisateur avec l'id 1
+
+if ($cleanedArray[1] === NULL || $cleanedArray[1] === "") {
+    echo 'Liste de la ressource '.$resource;
+    require_once('./'.$resource.'/list.php');
+    return;
+}
+
+$parameter = $cleanedArray[1];
+
+if ($parameter === 'create') {
+    echo 'Formulaire de création de la ressource '.$resource;
+    require_once('./'.$resource.'/create.php');
+    return;
+}
+
+if (is_numeric($parameter)) {
+    if ($cleanedArray[2] === NULL || $cleanedArray[2] === "") {
+        echo 'Affichage de la ressource '.$resource.' avec l\'id'.$parameter;
+        require_once('./'.$resource.'/show.php');
+        return;
+    }
+    if ($cleanedArray[2] === 'edit') {
+        echo 'Formulaire d\'édition de la ressource '.$resource.' avec l\'id '.$parameter;
+        require_once('./'.$resource.'/edit.php');
+        return;
+    }
+
+    if ($cleanedArray[2] === 'delete') {
+        echo 'Confirmation de suppression de la ressource '.$resource.' avec l\'id '.$parameter;
+        require_once('./'.$resource.'/delete.php');
+        return;
+    }
+}
+
+echo 'URL invalide';
---
+++ users/create.php
@@ -0,0 +1,26 @@
+<?php
+
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    // Affiche le formulaire vide
+    $html = '<form method="POST">';
+    $html .= '<input type="text" name="firstName" placeholder="First name" />';
+    $html .= '<input type="text" name="lastName" placeholder="Last name" />';
+    $html .= '<input type="email" name="email" placeholder="Email" />';
+    $html .= '<input type="password" name="password" placeholder="Password" />';
+    $html .= '<input type="submit" name="bSubmit" />';
+    $html .= '</form>';
+    echo $html;
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Enregistre les données du formulaire
+    // dans la base de données
+    $firstName = strip_tags($_POST['firstName']);
+    $lastName = strip_tags($_POST['lastName']);
+    $email = strip_tags($_POST['email']);
+    $password = strip_tags($_POST['password']);
+    
+    $q = $db->prepare('INSERT INTO users (firstName) VALUES (:firstName);');
+    $q->bindParam(':firstName', $firstName);
+    $q->execute();
+}
